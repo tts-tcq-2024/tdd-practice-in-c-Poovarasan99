9672e9b9ff69ee4173c500a7dac7fda01cb0b57c
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2024-07-08 12:59:53 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-int add(const char* input_string){
-  int sum;
+int add(char* input_string){
+  int sum = -1;
   if(*input_string == '\0'){
     sum = 0;
-  }
-  else{
-    sum = -1;
   }
   return sum;
 }
